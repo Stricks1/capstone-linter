5826b28ac3dd08eac47b7de497462237789f7597
Stricks1 <gmalheiross@gmail.com>
2020-05-22 19:47:09 -0300

    created RSpec for file validate angle brackets methods

--- spec/validate_file_spec.rb
+++ spec/validate_file_spec.rb
@@ -4,8 +4,10 @@ require './lib/validate_angle_brackets.rb'
 require './lib/error_found.rb'
 
 describe ValidateFile do
-  describe '#methodName' do
-
+  describe '#check_line' do
+    it 'call methods to check open tags, close tags and identation on that line' do
+    
+    end
   end
 end
 # rubocop:enable
