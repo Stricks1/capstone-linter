8ff6bfdf2fa512f9ab13ea8b4a2f1a062112750f
Stricks1 <gmalheiross@gmail.com>
2020-05-22 18:34:46 -0300

    add rspec for error found readers

---
+++ spec/validate_file_spec.rb
@@ -0,0 +1,11 @@
+# rubocop:disable
+require './lib/validate_file.rb'
+require './lib/validate_angle_brackets.rb'
+require './lib/error_found.rb'
+
+describe ValidateFile do
+  describe '#methodName' do
+
+  end
+end
+# rubocop:enable
